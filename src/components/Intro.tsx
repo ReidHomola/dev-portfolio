37eb3a910d45dd71f656825d610386a0205f569e
ReidHomola <reidghomola@gmail.com>
2024-10-29 23:15:28 -0500

    added some formatting to the intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -11,12 +11,14 @@ function Intro() {
                 As a dynamic software engineer with a proven track record, I
                 swiftly adapt to new technologies and excel in various languages
                 and frameworks. My passion for continuous learning drives me to
-                stay ahead in this ever-evolving field. I bring a wealth of
-                experience from the lumber and building materials industry,
-                where I honed my skills working with ERP systems. I have
-                developed robust analytics tools, integrated software solutions,
-                and automated numerous manual tasks to enhance operational
-                efficiency and data-driven decision-making.
+                stay ahead in this ever-evolving field.
+                <br />
+                <br />I bring a wealth of experience from the lumber and
+                building materials industry, where I honed my skills working
+                with ERP systems. I have developed robust analytics tools,
+                integrated software solutions, and automated numerous manual
+                tasks to enhance operational efficiency and data-driven
+                decision-making.
             </p>
         </div>
     );
